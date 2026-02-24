8d2344df6e2e5850896811deec4b88ccf17c2828
Ahmad El Hajj Chehade <69591723+Ahmad-El-Hajj-Chehade@users.noreply.github.com>
2026-02-24 08:54:57 +0100

    Update Services.tsx

--- dyoagency/src/components/Services.tsx
+++ dyoagency/src/components/Services.tsx
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 
 const services = [
   { title: "Booking Management", desc: "Strategic career development and placement on global mainstages and clubs.", image: "/images/management.png" },
-//  { title: "Tour Management", desc: "Full logistics, travel coordination, and technical rider fulfillment worldwide.", image: "/images/touring.JPG" },
- // { title: "Content Management", desc: "Visual identity, social media scaling, and professional media production.", image: "/images/media.jpg" },
+ { title: "Tour Management", desc: "Full logistics, travel coordination, and technical rider fulfillment worldwide.", image: "/images/touring.JPG" },
+  { title: "Content Management", desc: "Visual identity, social media scaling, and professional media production.", image: "/images/media.jpg" },
 ];
 
 export default function Services() {
